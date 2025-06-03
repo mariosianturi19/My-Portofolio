501c1c5145233cb065c6235c6b201519e89ba58e
Mario Sianturi <133350036+mariosianturi19@users.noreply.github.com>
2025-06-04 03:27:43 +0700

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  // Tambahkan base path jika menggunakan custom repository name
+  basePath: process.env.NODE_ENV === 'production' ? '/My-Portofolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/My-Portofolio/' : '',
 };
 
 module.exports = nextConfig;
